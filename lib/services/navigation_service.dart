6d667edb32aac7755bb93fe15edce4c3584e63b6
Patrick Witter <pwitterop123@gmail.com>
2021-10-11 22:53:57 -0500

    Created navigation service file

---
+++ lib/services/navigation_service.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/cupertino.dart';
+
+class NavigationService {
+  final GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
+
+  Future<dynamic> navigateTo(String routeName, {dynamic arguements}) {
+    return navigatorKey.currentState!
+        .pushNamed(routeName, arguments: arguements);
+  }
+
+  void navigateBack({dynamic arguements}) {
+    return (arguements != null)
+        ? navigatorKey.currentState!.pop(arguements)
+        : navigatorKey.currentState!.pop();
+  }
+
+  Future<dynamic> replaceAndNavigateTo(String routeName, {dynamic arguements}) {
+    return navigatorKey.currentState!
+        .pushReplacementNamed(routeName, arguments: arguements);
+  }
+}
