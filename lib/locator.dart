600b59bdcaf28cd1d91fe23f7f986c959380adff
Patrick Witter <pwitterop123@gmail.com>
2021-10-13 16:10:39 -0500

    Changed registration from Singleton to Lazy Singleton; registered FirebaseAuth and GoogleSigin

--- lib/locator.dart
+++ lib/locator.dart
@@ -1,4 +1,6 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:get_it/get_it.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 import 'package:upc_app/services/database_service.dart';
 import 'package:upc_app/services/firebase_service.dart';
 import 'package:upc_app/services/navigation_service.dart';
@@ -13,6 +15,10 @@ void setupLocator() {
   locator.registerFactory(() => MediatorScreenViewModel());
 
   // Services
-  locator.registerSingleton<DataBaseService>(FirebaseService());
-  locator.registerSingleton<NavigationService>(NavigationService());
+  locator.registerLazySingleton<DataBaseService>(() => FirebaseService());
+  locator.registerLazySingleton<NavigationService>(() => NavigationService());
+
+  //Firebase
+  locator.registerLazySingleton<FirebaseAuth>(() => FirebaseAuth.instance);
+  locator.registerLazySingleton<GoogleSignIn>(() => GoogleSignIn());
 }
