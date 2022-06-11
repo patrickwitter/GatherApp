1948d39617b7e8a82bf1df8ec455165b4dffdf52
Patrick Witter <pwitterop123@gmail.com>
2022-06-11 10:25:17 -0500

    refac: Added code to views

--- lib/views/after_auth/memberviewScreens/member_alert.dart
+++ lib/views/after_auth/memberviewScreens/member_alert.dart
@@ -1,12 +1,39 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:upc_app/viewmodels/memberview/memberalert_viewmodel.dart';
+import 'package:upc_app/views/base_view.dart';
 
 class MemberAlerts extends StatelessWidget {
-  const MemberAlerts({Key? key, required this.content}) : super(key: key);
-  final Widget content;
+  const MemberAlerts({
+    Key? key,
+  }) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: content,
-    );
+    return BaseView<MemeberAlertViewModel>(
+        onModelReady: (model) => model.initialize(),
+        builder: (context, model, child) {
+          return StreamBuilder<QuerySnapshot>(
+            stream: model.getAlerts,
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                print("has data");
+                print("${snapshot.data!.docs.length}");
+                return Text("has data ");
+              } else if (snapshot.hasError) {
+                print("error data \n ${snapshot.error}");
+
+                return Text(" error data");
+              } else {
+                return Center(
+                  child: Text(
+                    "Alerts",
+                    style: Theme.of(context).textTheme.caption,
+                  ),
+                );
+              }
+            },
+          );
+        });
   }
 }
--- lib/views/after_auth/memberviewScreens/member_view_home.dart
+++ lib/views/after_auth/memberviewScreens/member_view_home.dart
@@ -1,11 +1,56 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:upc_app/models/service.dart';
+import 'package:upc_app/viewmodels/memberview/member_home_viewmodel.dart';
+import 'package:upc_app/views/base_view.dart';
+import 'package:upc_app/widgets/customProgressIndicator.dart';
+import 'package:upc_app/widgets/rx_registerbutton.dart';
+import 'package:upc_app/widgets/serviceRegisterCard.dart';
 
 class MemberHome extends StatelessWidget {
-  const MemberHome({Key? key, required this.content}) : super(key: key);
+  const MemberHome({
+    Key? key,
+  }) : super(key: key);
 
-  final Widget content;
   @override
   Widget build(BuildContext context) {
-    return content;
+    return BaseView<MemberHomeViewModel>(
+        onModelReady: (model) => model.initialize(),
+        builder: (context, model, child) {
+          return StreamBuilder<QuerySnapshot>(
+              stream: model.serviceInfo,
+              builder: (context, snapshot) {
+                // print(" snap status ${snapshot.hasData}");
+                if (snapshot.hasData) {
+                  List<Service> servList;
+                  servList = model.getServiceList(
+                    snapshot.data,
+                  );
+                  return ListView.builder(
+                      itemCount: servList.length,
+                      itemBuilder: (BuildContext context, int index) {
+                        return ServiceRegisterCard(
+                          registerButton: RxRegisterButton(
+                            stream: model.isMemberRegistered(
+                              serv: servList[index],
+                            ),
+                            action: () => model.register(servList[index]),
+                            serv: servList[index],
+                          ),
+                          availSpace: servList[index].availSp,
+                          numAttend: servList[index].numAttend,
+                          servDate: servList[index].serviceDateFormat,
+                          servTime: servList[index].serviceTime.format(context),
+                        );
+                      });
+                } else if (!snapshot.hasData) {
+                  return Center(
+                    child: CustomCircularProgressIndicator(),
+                  );
+                } else {
+                  return Text("error");
+                }
+              });
+        });
   }
 }
